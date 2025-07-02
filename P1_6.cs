c908ae127d262635c37439c6a3b5e46b4bbfd0f3
Ankit Nakum <ankit.nakum2006@gmail.com>
2025-07-02 10:56:41 +0530

    P6 Done

--- P1_6.cs
+++ P1_6.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace CE_BTECH
 {
-    internal class P1_6
-    {
-    }
+        class P1_6
+        {
+            static void Main(string[] args)
+            {
+                int a, b, c, result;
+                string str;
+
+                Console.Write("Enter Number 1:");
+                str = Console.ReadLine();
+                a = Convert.ToInt32(str);
+
+                Console.Write("Enter Number 2:");
+                str = Console.ReadLine();
+                b = Convert.ToInt32(str);
+
+                Console.Write("Enter Number 3:");
+                str = Console.ReadLine();
+                c = Convert.ToInt32(str);
+
+                result = Sum(a, b, c);
+                Console.WriteLine("Sum:" + result);
+
+                Console.Read();
+            }
+
+            static int Sum(int x, int y, int z)
+            {
+                int res;
+                res = x + y + z;
+                return res;
+            }
+        }
 }
