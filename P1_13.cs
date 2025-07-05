4352ede4fe0dc061c39a7db08c5e530c1a1cefb0
Ankit Nakum <ankit.nakum2006@gmail.com>
2025-07-05 12:10:41 +0530

    up to 13

---
+++ P1_13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE_BTECH
+{
+    internal class P1_13
+    {
+        public static void Main(String[] args)
+        {
+            int a = 0,b=1,next;
+            Console.Write(a+" ");
+            Console.Write(b+" ");
+            for (int i=0; i<9; i++)
+            {
+                next = a + b;
+                Console.Write(next + " ");
+                a = b;
+                b = next;
+            }
+        }
+    }
+}
---
+++ P1_14.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CE_BTECH
+{
+    internal class P1_14
+    {
+        public static void Main(String[] args)
+        {
+            string s = "";
+            Console.WriteLine("Enter a number :");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            while (no != 0)
+            {
+                int digit = no % 10;
+                if (digit == 0)
+                {
+                    s += "Zero ";
+                }
+                else if (digit == 1)
+                {
+                    s += "One ";
+                }
+                else if (digit == 2)
+                {
+                    s += "Two ";
+                }
+                else if (digit == 3)
+                {
+                    s += "Three ";
+                }
+                else if (digit == 4)
+                {
+                    s += "Four ";
+                }
+                else if (digit == 5)
+                {
+                    s += "Five ";
+                }
+                else if (digit == 6)
+                {
+                    s += "Six ";
+                }
+                else if (digit == 7)
+                {
+                    s += "Seven ";
+                }
+                else if (digit == 8)
+                {
+                    s += "Eight ";
+                }
+                else if (digit == 9)
+                {
+                    s += "Nine ";
+                }
+                no = no / 10;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
