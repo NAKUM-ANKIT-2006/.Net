febd1545e3f363d2a4521f0ab234cf246d9f24d0
Ankit Nakum <ankit.nakum2006@gmail.com>
2025-07-10 15:09:37 +0530

    done upto 19 in tutorial-1

--- P1_19.cs
+++ P1_19.cs
@@ -13,7 +13,18 @@ namespace CE_BTECH
     {
         public static void Main()
         {
-
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 4 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Practice.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace CE_BTECH
+{
+    internal class Practice
+    {
+        public static void Main()
+        {
+            int a = 5;
+            int b = 5;
+            int result = a++ + ++b + ++b + ++a + a++ + a++;
+            Console.WriteLine("the value of result is: " + result);
+
+            int c = 2;
+
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine("c is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("c is 2");
+                    break;
+            }
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Using 2 For Loop");
+            Console.WriteLine();
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 4; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Using If.not use 2 time for loop");
+            Console.WriteLine();
+
+
+            for (int i=1; i<=9; i++)
+            {
+                //int k = i;
+                int k;
+                if (i <= 5)
+                {
+                    k = i;
+                }
+                else
+                {
+                    k = 10 - i;
+                }
+                for (int j = 1; j <= k; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Using If.not use 2 time for loop and in 1 line condition");
+            Console.WriteLine();
+
+
+            for (int i = 1; i <= 9; i++)
+            {
+                int k = i<=5 ? i:10 - i;
+                for (int j = 1; j <= k; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
